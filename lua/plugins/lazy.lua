e4abbb7119bcbecc5baf237904fb191af143efef
Svyatoslav <sakairlinux@gmail.com>
2026-01-11 17:19:42 +0300

    Add files via upload

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -62,8 +62,44 @@ require("lazy").setup({
       -- …etc.
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  },
- 
+  },	
+
+
+	{
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      " ____        _         _      ",
+      " / ___|  __ _| | ____ _(_) __ ",
+      "\\___ \\ / _` | |/ / _` | | __|",
+      " ___) | (_| |   < (_| | | |   ",
+      " |____/ \\__,_|_|\\_\\__,_|_|_|  ",
+    }
+
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("c", "  Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("q", "  Quit", ":qa<CR>"),
+    }
+
+    dashboard.section.footer.val = {
+      "GitHub: https://github.com/Saka1r"
+    }
+
+    -- optional highlights
+    vim.cmd("hi AlphaHeader guifg=#FFD700")
+    dashboard.section.header.opts.hl = "AlphaHeader"
+
+    alpha.setup(dashboard.opts)
+  end
+}
+
 }})
 
 
