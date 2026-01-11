3cafbfc5f639c63a6f7849cbdbf3414e83866e69
Svyatoslav <sakairlinux@gmail.com>
2026-01-11 17:04:43 +0300

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
 print("Hello in NeoVim")
 
+
 require("plugins.lazy")
 
 require("config")
 require("keymap")
---require("plugins.mason")
+
 --require("lsp")
 
-
 vim.cmd.colorscheme "catppuccin"
+
+
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,5 @@
+vim.lsp.enable('pyright')
+vim.lsp.enable('clangd')
+vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('lua_ls')
+
